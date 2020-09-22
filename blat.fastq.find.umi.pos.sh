f8986b0b07317c4c6ea5c0aadc47c3a7becf9ab7
jyaomda <63140676+jyaomda@users.noreply.github.com>
2020-09-22 14:06:18 -0500

    Update blat.fastq.find.umi.pos.sh

--- blat.fastq.find.umi.pos.sh
+++ blat.fastq.find.umi.pos.sh
@@ -16,3 +16,10 @@
     writeLines(y[i], fh)}; close(fh)
     
 ### figure out where UMI seqs are ###
+blat panel.3.for.blat.fasta ../YY-ctDNA-MCL_PM1304_189/Sample_1224172/10K.record.fasta blat.10K.1224172.psl
+table(a$size)
+  81   82   83   84   85   86   87   88   89   90   91   92   93   94   95   96   97   98   99  100
+  25   24   17   28   17   33   18   23   24   19   36   41   41   27 2332 2993 1018  281  100  789
+## this means although the UMI tag is 3-bp at 5'-end, some has additional matches (98-100),
+## majority are actually having additional 1-2bp unmatching sequences before target sequence
+## nevertheless, the right way is to extract raw.R1/2.fastq then combine the 3-bp UMI tag
