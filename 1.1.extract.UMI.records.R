12156cb6c774f0d4c697ff1b80a933505338ddc4
jyaomda <63140676+jyaomda@users.noreply.github.com>
2020-09-23 11:02:17 -0500

    Update 1.1.extract.UMI.records.R

--- 1.1.extract.UMI.records.R
+++ 1.1.extract.UMI.records.R
@@ -18,76 +18,12 @@
 ##       1 2615012 2615012 54865344 
 ##  from here we know ambiguous UMI 1.2% can be removed
 
-add_UMI_record = function(s1, s2){
-  ## input:
-  ## s1/2: PE sequence reads
-  ## procedure:
-  ## extract UMI
-  ## if UMI in result
-  ##   append s1/2 sequences (UMI trimmed)
-  ## else
-  ##   initiate UMI list in result
-  ##   initiate s1/2 record
-  size = nchar(s1)
-  umi = paste0(substr(s1,1,3), substr(s2,1,3))
-  if(umi %in% names(result)){
-    result[[umi]]$s1 <<- c(result[[umi]]$s1, substr(s1, 4, size))
-    result[[umi]]$s2 <<- c(result[[umi]]$s2, substr(s2, 4, size))
-  } else {
-    result[[umi]]$s1 <<- substr(s1, 4, size)
-    result[[umi]]$s2 <<- substr(s2, 4, size)
-  }
-}
-
-make_UMI_fasta = function(u){
-  ## input:
-  ## u: UMI record list of s1/s2 PE sequences w/ UMI trimmed
-  ## procedure:
-  ## concatenate s1/s2, make table
-  ## for each unique s1/s2
-  ##   write fasta >line with freq
-  ##   write fasta sequence s1/s2
-  ## return fasta string
-  
-  s12 = paste0(u$s1, u$s2)
-  a = table(s12); f = NULL
-  for(i in 1:length(a)){
-    f = c(f, paste0('>seq_', i, '_count=', a[i]))
-    f = c(f, names(a[i]))
-  }
-  return(f)
-}
-result = list()
-read_block = 4000000; n = 0
-fh1 = gzfile('raw.R1.fastq.gz', 'r')
-fh2 = gzfile('raw.R2.fastq.gz', 'r')
-
-while(1){
-  x1 = readLines(fh1, read_block)
-  if(length(x1) == 0){ break }
-  x2 = readLines(fh2, read_block)
-  seq1 = x1[seq(2, read_block, 4)]
-  seq2 = x2[seq(2, read_block, 4)]
-  for(i in 1:(read_block/4)){
-    add_UMI_record(seq1[i], seq2[i])
-  }
-  n = n + 1
-  print(paste0(n,'M PE reads processed ... ', length(result),' UMIs found'))
-}
-close(fh1); close(fh2)
-
-dir.create('umi')
-for(u in names(result)){
-  savename = paste0('umi/', u, '.fasta')
-  writeLines(make_UMI_fasta(result[[u]]), savename)
-}
-
-### version 2 ###
+### version 2.1 ###
 add_UMI_record = function(s1, s2){
   if(is.na(s1)){ return() }
-  size = nchar(s1)
+  ### change size to 100 and skip nchr() ###
   umi = paste0(substr(s1,1,3), substr(s2,1,3))
-  pe = paste0(substr(s1,4,size), substr(s2,4,size))
+  pe = paste0(substr(s1,4,100), substr(s2,4,100))
   result[[umi]] <<- c(result[[umi]], pe)
 }
 
@@ -120,7 +56,10 @@ while(1){
 close(fh1); close(fh2)
 
 dir.create('umi')
-for(u in names(result)){
+pos = grep('N', names(result))
+good_umi = names(result)[setdiff(1:length(result), pos)]
+
+for(u in good_umi){
   savename = paste0('umi/', u, '.fasta')
   writeLines(make_UMI_fasta(result[[u]]), savename)
 }
